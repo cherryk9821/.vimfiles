31524e26d2f38456298095dc21dfb51bd45db333
cherryk9821 <cherryk9821@gmail.com>
2018-11-05 19:24:33 +0900

    change setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@ mkdir -p $HOME/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 ln -sf $HOME/.vimfiles/vimrc_vundle $HOME/.vimrc
 vim +PluginInstall +qall
+vim +GoInstallBinaries +qall
 ln -sf $HOME/.vimfiles/vimrc $HOME/.vimrc
